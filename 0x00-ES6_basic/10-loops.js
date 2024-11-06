d27d63ddb87019d98625bd9b6591786012f260e3
B. Lamine <medlamine.bellilet@outlook.com>
2024-11-06 21:19:05 +0000

    10-loops.js: use for..of loop

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let newArray = [];
+  for (const value of array) {
+    newArray = [...newArray, appendString + value];
+  }
+
+  return newArray;
+}
